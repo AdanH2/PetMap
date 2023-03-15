64ac569b009a8a0edc97eb5f1905243d615a0040
alexish43 <alexish1943@gmail.com>
2023-03-14 22:40:56 -0700

    message

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <div style={{ textAlign: 'center' }}>
                 <div className="title">
-                    Welcome to PetMap!!!
+                    Welcome to PetMap
                 </div>
             </div>
         </div>
